655837234b70f3a273bd42a4f5ab1b5c58d25141
Fuhaoyi <fhy11235813@gmail.com>
2019-12-27 23:26:28 +0800

    Update ACEP_model_train.py

--- ACME_codes/ACEP_model_train.py
+++ ACME_codes/ACEP_model_train.py
@@ -210,7 +210,7 @@ def ConvertSequence2Feature(sequence_data, pssmdir):
 #--------------------------------------------------------
 
 
-data_all = pd.read_csv('pssm_files0_3555_all\\seq_all_data.csv', index_col=0)
+data_all = pd.read_csv('AMPs_Experiment_Dataset\\AMP_sequecnces\\seq_all_data.csv', index_col=0)
 
 train = data_all.iloc[0:1424].reset_index(drop=True)
 tune = data_all.iloc[1424:2132].reset_index(drop=True)
@@ -219,11 +219,11 @@ train_tune = data_all.iloc[0:2132].reset_index(drop=True)
 train_tune_test = data_all.iloc[0:3556].reset_index(drop=True)
 
 
-x_train_index, x_train_length, x_train_pssm, x_train_onehot, x_train_aac, y_train = ConvertSequence2Feature(sequence_data=train, pssmdir=os.path.join('pssm_files0_3555_all','cut','train'))
-x_test_index, x_test_length, x_test_pssm, x_test_onehot, x_test_aac, y_test = ConvertSequence2Feature(sequence_data=test, pssmdir=os.path.join('pssm_files0_3555_all','cut','test'))
-x_tune_index, x_tune_length, x_tune_pssm, x_tune_onehot, x_tune_aac, y_tune = ConvertSequence2Feature(sequence_data=train, pssmdir=os.path.join('pssm_files0_3555_all','cut','train'))
-x_train_tune_index, x_train_tune_length, x_train_tune_pssm, x_train_tune_onehot,x_train_tune_aac,y_train_tune= ConvertSequence2Feature(sequence_data=train_tune, pssmdir=os.path.join('pssm_files0_3555_all','cut','train_tune'))
-x_train_tune_test_index, x_train_tune_test_length, x_train_tune_test_pssm, x_train_tune_test_onehot,x_train_tune_test_aac,y_train_tune_test= ConvertSequence2Feature(sequence_data=train_tune_test, pssmdir=os.path.join('pssm_files0_3555_all','cut','train_tune_test'))
+x_train_index, x_train_length, x_train_pssm, x_train_onehot,x_train_aac,y_train= ConvertSequence2Feature(sequence_data=train, pssmdir=os.path.join('AMPs_Experiment_Dataset','PSSM_files','train'))
+x_test_index, x_test_length, x_test_pssm, x_test_onehot, x_test_aac, y_test = ConvertSequence2Feature(sequence_data=test, pssmdir=os.path.join('AMPs_Experiment_Dataset','PSSM_files','test'))
+x_tune_index, x_tune_length, x_tune_pssm, x_tune_onehot,x_tune_aac,y_tune= ConvertSequence2Feature(sequence_data=tune, pssmdir=os.path.join('AMPs_Experiment_Dataset','PSSM_files','tune'))
+x_train_tune_index, x_train_tune_length, x_train_tune_pssm, x_train_tune_onehot,x_train_tune_aac,y_train_tune= ConvertSequence2Feature(sequence_data=train_tune, pssmdir=os.path.join('AMPs_Experiment_Dataset','PSSM_files','train_tune'))
+x_train_tune_test_index, x_train_tune_test_length, x_train_tune_test_pssm, x_train_tune_test_onehot,x_train_tune_test_aac,y_train_tune_test= ConvertSequence2Feature(sequence_data=train_tune_test, pssmdir=os.path.join('AMPs_Experiment_Dataset','PSSM_files','train_tune_test'))
 
 
 
@@ -341,7 +341,7 @@ def creat_model():
 #-----------Training history visualization---------
 model = creat_model()
 history = model.fit([x_train_tune_test_aac,x_train_tune_test_onehot, x_train_tune_test_pssm], y_train_tune_test,
-                    validation_split=0.4, epochs=40, batch_size=16, verbose=1)
+                    validation_split=0.4, epochs=1, batch_size=16, verbose=30)
 
 plt.plot(history.history['acc'],color='firebrick', linestyle='-', marker='o')
 plt.plot(history.history['val_acc'],color='forestgreen',linestyle='-', marker='o')
@@ -350,7 +350,7 @@ plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.grid(linestyle='--')
-plt.savefig('supplementary_data//fig_s2_2.svg',dpi=600,format='svg')
+#plt.savefig('resuts//fig_s2_2.svg',dpi=600,format='svg')
 plt.show()
 
 
@@ -402,7 +402,7 @@ def evaluate_model(x_model):
     AUC = metrics.roc_auc_score(y_test, y_pred_prob)
     print('ROC Curves and Area Under the Curve (AUC):', AUC)
 
-evaluate_model(model0_1)
+evaluate_model(history)
 
 from keras import backend
 import time
