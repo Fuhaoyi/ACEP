14724313421f42075806e6605a28796dbe5b09cd
Fuhaoyi <fhy11235813@gmail.com>
2019-12-31 16:25:22 +0800

    Update ACEP_prediction.py

--- ACME_codes/ACEP_prediction.py
+++ ACME_codes/ACEP_prediction.py
@@ -1,238 +1,13 @@
-
-from EmbeddingRST import EmbeddingRST_model
-
-
-from keras.models import Sequential, Model
-from keras.layers import Input, Dense, Dropout, Activation, Lambda, Permute, Reshape, Flatten, Masking
-from keras.layers import Multiply, Dot, RepeatVector,Concatenate
-from keras.layers import Embedding
-from keras.layers import LSTM,Bidirectional,BatchNormalization
-from keras.layers import Conv1D, MaxPooling1D
-from keras.layers import TimeDistributed,Add
-
-import matplotlib.pyplot as plt
-from keras.utils import plot_model
-from keras.callbacks import TensorBoard
-
 import pandas as pd
 import numpy as np
-#--------------------------------------------------------
-
 import os
-import math
+from EmbeddingRST import EmbeddingRST_model
+from feature_generation_pre import ConvertSequence2Feature
+from sklearn.preprocessing import binarize
 
 
-def getpssm(filename):
-    f = open(filename, 'r')
-    f.readline()
-    f.readline()
-    f.readline()
-    linelist = f.readlines()
-    pssm = []
-    for i in linelist:
-        if i!='\n':
-            oneline = i.split()
-            m = []
-            m.append(oneline[1])
-            #m.extend([int(i) for i in oneline[22:42]])   #--------------------------
-            m.extend([int(i) for i in oneline[2:22]])
-            pssm.append(m)
-        else:
-            break
-    f.close()
-
-    pssmdf = pd.DataFrame(pssm,columns=['seq','A','R','N','D','C','Q','E','G','H','I',
-                                        'L','K','M','F','P','S','T','W','Y','V'])
-    col = pssmdf.columns.insert(1, 'X')
-    pssmdf = pssmdf.reindex(columns=col, fill_value=0)
-
-    return pssmdf
-
-
-def getpssmlist(dir):
-    filenamelist = os.listdir(dir)
-    pssmlist = []
-    pssm_file_list = []
-    for i in filenamelist:
-        filename = os.path.join(dir,i)
-        pssmlist.append(getpssm(filename))
-        pssm_file_list.append(i)
-    print('pssmlist is ok')
-    print(len(pssm_file_list))
-    #print(pssm_file_list)
-    return  pssmlist, pssm_file_list
-
-
-
-
-def psssmfeature(sequence_index,one_sequence, label, pssm_matrix_list):
-
-    str1 = one_sequence
-    padding_vst_pssm = -1
-    sequence_pssm = []
-    str2 = ''.join(pssm_matrix_list[sequence_index].iloc[:, 0].values)
-    if str1 == str2:
-        pssm = pssm_matrix_list[sequence_index].iloc[:, 1:22].values
-        padding = np.zeros((200 - pssm.shape[0], 21), dtype=np.float32)
-        padding_vst_pssm = np.vstack([padding, pssm])
-        sequence_pssm = [sequence_index, pssm_matrix_list[sequence_index]]
-
-    if type(padding_vst_pssm) == type(-1):
-        print('error pssm in empty')
-        print(sequence_index)
-        print(one_sequence)
-
-    sequence_label = label
-    sequence_index_out = sequence_index
-    return sequence_index_out, padding_vst_pssm, sequence_label, sequence_pssm   #numpy  shape:(None,200,21)  (None,)
-
-
-
-def encode2numerical(sequence_index, one_sequence, label):
-    dict1 = {'X':0,
-             'A':1,
-             'R':2,
-             'N':3,
-             'D':4,
-             'C':5,
-             'Q':6,
-             'E':7,
-             'G':8,
-             'H':9,
-             'I':10,
-             'L':11,
-             'K':12,
-             'M':13,
-             'F':14,
-             'P':15,
-             'S':16,
-             'T':17,
-             'W':18,
-             'Y':19,
-             'V':20
-             }
-
-    str1 = one_sequence
-    str1 = 'X' * (200 - len(str1)) + str1
-    str2 = ''
-    for j in str1:
-        if j not in dict1.keys():
-            j = 'X'
-        str2 =str2 + str(dict1[j]) + ' '
-
-    sequence_num = str2.split()
-    sequence_num = [int(k) for k in sequence_num]
-    sequence_label = label
-    sequence_index_out = sequence_index
-
-    return sequence_index_out ,sequence_num, sequence_label
-
-
-
-def one_hot_vector(sequence_index, one_sequence, label):
-    sequence_index_out, sequence_num, sequence_label = encode2numerical(sequence_index, one_sequence, label)
-    one_hot_matrix = np.zeros((200, 21), dtype=np.float32)
-    for i in range(200):
-        if sequence_num[i]!=0:
-            one_hot_matrix[i,sequence_num[i]] = 1.0
-
-    #return sequence_index_out, one_hot_matrix, sequence_label   #numpy   shape:(None,200,21) (None,)
-    return one_hot_matrix   #numpy   shape:(None,200,21) (None,)
-
-def AAC_ft(sequence_index_x, one_sequence_x, label_x):
-    dict1 = {'X': 0,
-             'A': 1,
-             'R': 2,
-             'N': 3,
-             'D': 4,
-             'C': 5,
-             'Q': 6,
-             'E': 7,
-             'G': 8,
-             'H': 9,
-             'I': 10,
-             'L': 11,
-             'K': 12,
-             'M': 13,
-             'F': 14,
-             'P': 15,
-             'S': 16,
-             'T': 17,
-             'W': 18,
-             'Y': 19,
-             'V': 20
-             }
-
-    str1 = one_sequence_x
-    str2 = ''
-    for j in str1:
-        if j not in dict1.keys():
-            j = 'X'
-        str2 = str2 + str(dict1[j]) + ' '
-
-    sequence_num = str2.split()
-    sequence_num = [int(k) for k in sequence_num]
-    sequence_aac = np.zeros((21,),dtype=np.float32)
-    for i in range(21):
-        count = 0
-        for j in sequence_num:
-            if i == j:
-                count = count+1
-        sequence_aac[i] = count
-    #sequence_aac = sequence_aac/len(sequence_num)
-    sequence_label = label_x
-    sequence_index_out = sequence_index_x
-
-    #return sequence_index_out, sequence_aac, sequence_label
-    return sequence_aac
-
-
-def ConvertSequence2Feature(sequence_data, pssmdir):
-    sequence_count = sequence_data.shape[0]
-    #print(sequence_data)
-    feature_pssm = np.zeros((sequence_count, 200, 21), dtype=np.float32)
-    feature_onehot = np.zeros((sequence_count, 200, 21), dtype=np.float32)
-    feature_aac = np.zeros((sequence_count, 21), dtype=np.float32)
-
-    label_list = np.zeros((sequence_count), dtype=np.int32)
-    index_out = np.zeros((sequence_count), dtype=np.int32)
-    pssm_list, pssm_file_list = getpssmlist(pssmdir)
-    #print(pssm_file_list)
-    length_list = []
-    for i in range(sequence_count):
-        #length_list.append(len(sequence_data.iloc[i, 0])/sequence_data.iloc[i, 2])    #------------------------
-        length_list.append(len(sequence_data.iloc[i, 0]))
-    count_1 = 0
-    for i in range(sequence_count):
-        one_sequence = sequence_data.iloc[i,0]
-        label = sequence_data.iloc[i,2]
-        index_out[i], feature_pssm[i], label_list[i], sequence_pssm = psssmfeature(i, one_sequence, label, pssm_list)
-        feature_onehot[i] = one_hot_vector(i, one_sequence, label)
-        feature_aac[i] = AAC_ft(i, one_sequence, label)
-        count_1 = count_1 +1
-    print('total    ',count_1)
-    return index_out, length_list, feature_pssm, feature_onehot, feature_aac, label_list
-
-
-#--------------------------------------------------------
-
-
-
-data_all = pd.read_csv('AMPs_Experiment_Dataset\\AMP_sequecnces\\seq_all_data.csv', index_col=0)
-
-train = data_all.iloc[0:1424].reset_index(drop=True)
-tune = data_all.iloc[1424:2132].reset_index(drop=True)
-test = data_all.iloc[2132:3556].reset_index(drop=True)
-train_tune = data_all.iloc[0:2132].reset_index(drop=True)
-train_tune_test = data_all.iloc[0:3556].reset_index(drop=True)
-
-
-
-x_train_index, x_train_length, x_train_pssm, x_train_onehot,x_train_aac,y_train= ConvertSequence2Feature(sequence_data=train, pssmdir=os.path.join('AMPs_Experiment_Dataset','PSSM_files','train'))
-x_test_index, x_test_length, x_test_pssm, x_test_onehot, x_test_aac, y_test = ConvertSequence2Feature(sequence_data=test, pssmdir=os.path.join('AMPs_Experiment_Dataset','PSSM_files','test'))
-x_tune_index, x_tune_length, x_tune_pssm, x_tune_onehot,x_tune_aac,y_tune= ConvertSequence2Feature(sequence_data=tune, pssmdir=os.path.join('AMPs_Experiment_Dataset','PSSM_files','tune'))
-x_train_tune_index, x_train_tune_length, x_train_tune_pssm, x_train_tune_onehot,x_train_tune_aac,y_train_tune= ConvertSequence2Feature(sequence_data=train_tune, pssmdir=os.path.join('AMPs_Experiment_Dataset','PSSM_files','train_tune'))
-x_train_tune_test_index, x_train_tune_test_length, x_train_tune_test_pssm, x_train_tune_test_onehot,x_train_tune_test_aac,y_train_tune_test= ConvertSequence2Feature(sequence_data=train_tune_test, pssmdir=os.path.join('AMPs_Experiment_Dataset','PSSM_files','train_tune_test'))
+test = pd.read_csv('AMP_prediction\\inputs\\sequences.csv')
+x_test_index, x_test_length, x_test_pssm, x_test_onehot, x_test_aac = ConvertSequence2Feature(sequence_data=test, pssmdir=os.path.join('AMP_prediction','inputs','PSSM_files'))
 
 
 
@@ -240,10 +15,11 @@ from keras.models import load_model
 
 def save_pred_prob():
     model_best = load_model('models\\ACEP_model_train_241_9304.h5', custom_objects={'EmbeddingRST_model': EmbeddingRST_model})
-    # #print(model.summary())
+    print(model_best.summary())
     y_pred_prob = model_best.predict([x_test_aac, x_test_onehot, x_test_pssm])
-    y_pred_prob_np = np.hstack([x_test_index.reshape((-1,1)), test.iloc[:,0].values.reshape(-1,1), y_pred_prob, y_test.reshape((-1,1))])
-    y_pred_prob_pd = pd.DataFrame(y_pred_prob_np, columns=['test_index', 'test_seq', 'pred_prob', 'test_label'])
-    y_pred_prob_pd.to_csv('results\\ACEP_model_prediction.csv')
+    y_pred_class = binarize([y_pred_prob.flatten()], 0.5)[0]
+    y_pred_prob_np = np.hstack([x_test_index.reshape((-1,1)), test.iloc[:,0].values.reshape(-1,1), y_pred_prob,y_pred_class.reshape((-1,1))])
+    y_pred_prob_pd = pd.DataFrame(y_pred_prob_np, columns=['index', 'sequence', 'probability','AMP/nonAMP'])
+    y_pred_prob_pd.to_csv('AMP_prediction\\outputs\\outputs.csv')
 
 save_pred_prob()
