957d0fdc16f6b5b96754fa9b6582416bafc634b3
Fuhaoyi <fhy11235813@gmail.com>
2020-07-06 20:11:00 +0800

    Update localBLASTgetPSSMs.py

--- ACME_codes/localBLASTgetPSSMs/localBLASTgetPSSMs.py
+++ ACME_codes/localBLASTgetPSSMs/localBLASTgetPSSMs.py
@@ -1,13 +1,13 @@
 import os
 
-def command_pssm(database_nr, one_query, out_file, out_pssm):
-    os.system('E:/blast-2.9.0+/bin/psiblast  \
+def command_pssm(blast_path, database_nr, one_query, out_file, out_pssm):
+    os.system('%s \
                 -db %s \
                 -query %s \
                 -num_iterations 3 \
                 -evalue 1e-3 \
                 -out %s \
-                -out_ascii_pssm %s' %(database_nr, one_query, out_file, out_pssm))
+                -out_ascii_pssm %s' %(blast_path,database_nr, one_query, out_file, out_pssm))
 
 def getOneSequence(query_seqs):
     input_file = open(query_seqs,'r')
@@ -24,18 +24,18 @@ def getOneSequence(query_seqs):
         i = i+2
         count = count+1
 
-def callBLASTgetPSSMs(path1, path2):
+def callBLASTgetPSSMs(path0, path1, path2):
     getOneSequence('queryseq.fasta')
     dir = 'sequences/'
     dir_list = os.listdir(dir)
-    query_database = path1
     for i in dir_list:
         print(i)
         one_query_name = path2 + 'sequences/' + i
         out_file_name =path2 + 'results/' + i.split('.')[0]+'.out'
         out_pssm_name = path2 + 'pssms/' + i.split('.')[0] + '.pssm'
-        command_pssm(query_database, one_query_name, out_file_name, out_pssm_name)
+        command_pssm(path0, path1, one_query_name, out_file_name, out_pssm_name)
         print(out_pssm_name)
 
-callBLASTgetPSSMs(path1='E:/blast-2.9.0+/bin/uniref90.db',
-                  path2='E:/ACEP/ACME_codes/localBLASTgetPSSMs/')
+callBLASTgetPSSMs(path0 ='E:/blast-2.9.0+/bin/psiblast',
+                  path1 ='E:/blast-2.9.0+/bin/uniref90.db',
+                  path2 ='E:/ACEP/ACME_codes/localBLASTgetPSSMs/')
